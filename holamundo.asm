212d3931ea441fe3ad12d3e6d03f8c4a09eda775
Mario Ch <chmario07@gmail.com>
2014-11-24 21:59:44 -0600

    Write in a file

---
+++ holamundo.asm
@@ -0,0 +1,26 @@
+%include "io.mac"
+
+   section .text
+
+    global _start
+
+        _start:
+                    mov EAX, 8
+                    mov EBX, filename
+                    mov ECX, 0700
+                    int 0x80
+                    mov EBX, EAX
+                    mov EAX, 4
+                    mov ECX, texto
+                    mov EDX, textlen
+                    int 0x80
+                    mov EAX, 6
+                    int 0x80
+                    mov eax, 1
+                    int 0x80
+
+    section .data
+
+        filename db "./output.txt", 0
+        texto db "Umpa, oscar ,mario", 0
+        textlen equ $ - texto
