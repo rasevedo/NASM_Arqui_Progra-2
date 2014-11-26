ccc628986714479607bae98d06620b61ac4bc9c9
Mario Ch <chmario07@gmail.com>
2014-11-26 11:01:50 -0600

    Actualizacion por error.

--- archivo.asm
+++ archivo.asm
@@ -3,8 +3,8 @@
 .DATA
 	msg: db "Bienvenido al generador de ADN, ingrese la cantidad que desea: ",0
 	msg_2: db "Digite el nombre del archivo a generar y no olvide la extensión (.adn): ",0
-	msg_3: db "Su archivo con extensión .adn ha sido creado porfavor si desea crear otro archivo corra nuevamente el programa",0
-	;;;;;preg: db "Desea crear otra secuencia de ADN?(S/n): ",0;;;;; No sirve se cae
+	msg_3: db "Su archivo con extensión .adn ha sido creado",0
+	preg: db "Desea crear otra secuencia de ADN?(S/n): ",0
 	fd: dd 0
 
 .UDATA
@@ -23,7 +23,7 @@ main:
 	call lecturaDatos
 	nwln
 	call creaArchivo
-	;;;;;;;;;;;;;;;call pregunta
+	;call pregunta
 	call salir
 
 
@@ -51,23 +51,25 @@ Generador:
 
 random:
 	
-	xor EAX, EAX		;limpio el eax					
-	RDTSC			;para el random	
+	xor EAX, EAX						
+	RDTSC				
 	and EAX, 0FFH		
-	mov EBX, 4		; muevo al ebx un 4 
-	xor EDX, EDX		; limpio el edx 
-	div EBX			; divido (modulo) entre 4
+	mov EBX, 4			; muevo al ebx un 4 
+	xor EDX, EDX		; limpio el edx para aplicar modulo
+	div EBX				; divido (modulo) entre 4
 	mov EAX, EAX 		; paso el resultado random(0, 1, 2, 3) al eax
 
 	
-	cmp eax, 0		;aca en caso de que el random sea 0 es adenina, 1 es citocina, 2 es timina y 3 es guanina 
+	cmp eax, 0			;aca en caso de que el random sea 0 es adenina, 1 es citocina, 2 es timina y 3 es guanina 
+
 	je adenina			
-	cmp EAX, 1
+	cmp eax, 1
 	je citosina
-	cmp EAX, 2
+	cmp eax, 2
 	je timina
-	jmp guanina
-	ret
+	cmp eax, 3
+	je guanina
+	;ret
 
 adenina:
 	mov byte[secuencia],"A"
@@ -88,22 +90,21 @@ guanina:
 
 creaArchivo:
 
-	mov EAX, 8 			; Crea el Archivo
-        mov EBX, filename 		; Asigna el nombre del Archivo
+		mov EAX, 8 			; Crea el Archivo
+        mov EBX, filename 	; Asigna el nombre del Archivo
         mov ECX, 644O 
-        int 80h 			;interrupcion al sistema
+        int 80h 
         mov [fd], eax 	
-	mov EAX, 4 			; Escribe en el Archivo
+		mov EAX, 4 			; Escribe en el Archivo
         mov EBX, [fd],
-        mov ECX, cadena			
+        mov ECX, cadena
         mov EDX, [tamTEXT]
-        int 80h				;interrupcion al sistema
-	mov EAX, 6 			; close
+        int 80h
+		mov EAX, 6 			; close
         mov EBX, [fd]
-        int 80h				;interrupcion al sistema
-	ret
+        int 80h
+		ret
 
-;;;;;;;;esta parte preguntaba si el usuario queria volver a correr e lprograma sin cerrarlo pero se cae;;;;;;;;;;;;;;;;	
 pregunta:
 
 	PutStr preg
@@ -112,7 +113,6 @@ pregunta:
 	je main
 	jne salir
 	ret
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 salir:
      .EXIT
