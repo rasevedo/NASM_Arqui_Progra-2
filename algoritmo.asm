a5a0f39867899f08f45a9098d205eb0c702eb532
rasevedo <rasevedo9@gmail.com>
2014-11-25 21:10:40 -0600

    Algoritmo Matriz SW
    Este realiza la matriz para el algoritmo de Smith Waterman, en teoría crea la matriz pero la deja rellenada todo en cero.....

--- algoritmo.asm
+++ algoritmo.asm
@@ -6,7 +6,7 @@
 	sys_open 		equ 	5	
 	sys_close 		equ 	6
 	sys_create 		equ 	8
-	stdin 			equ		0
+	stdin 			equ	0
 	stdout 			equ 	1
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
@@ -45,7 +45,6 @@ archivo_1	resb	LenArchivo_1
 LenArchivo_2	equ	1000000
 archivo_2	resb	LenArchivo_2
 
-;-------------------------------------------------------------------------------------------------------
 
 SECTION .text
 
@@ -56,102 +55,102 @@ _start:
 
 	nop
 
-;;;preguntar por el nombre del primer archivo	
+;;;preguntar por el nombre del primer archivo;;;	
 Preguntar_Nombre_1:
-	mov ecx, msg1		;muevo al ecx el mensaje al usuario
-	mov edx, lenmsg1	;muevo al edx el largo
-	call MostrarTexto	;llamo al procedimiento de imprimir en pantalla
+	mov 	ECX, msg1		;muevo al ecx el mensaje al usuario
+	mov 	EDX, lenmsg1		;muevo al edx el largo
+	call MostrarTexto		;llamo al procedimiento 
 
 	;leo de usuario un texto
-	mov ecx, input_1	;muevo al ecx el puntero del buffer de almacenamiento
+	mov 	ECX, input_1		;muevo al ecx el puntero del buffer de almacenamiento
 
-	mov edx, lenInput_1		;muevo la catidad de caracteres maxima q puedo tener
+	mov 	EDX, lenInput_1		;muevo la catidad de caracteres maxima q puedo tener
 
-	call LeerTexto		;llamo al procedimiento de recibir de teclado
+	call LeerTexto			;llamo al procedimiento de recibir de teclado
 
-	dec eax				;en el eax tengo el largo del mensaje, lo decremento
-	mov byte[input_1+eax],0h	;cambio el ultimo caracter por un null
+	dec EAX				;en el eax tengo el largo del mensaje, lo decremento
+	mov byte[input_1+EAX],0h	;cambio el ultimo caracter por un null
 	
 
-;;;;abrir el archivo 
+;;;;abrir el archivo;;;;; 
 .Abrir_archivo_1:			
-	mov eax, sys_open		;muevo al eax la directiva de abrir archivo
-	mov ebx, input_1  		;muevo al ebx el nombre de lo que quiero abrir
+	mov 	EAX, sys_open			;muevo al eax la directiva de abrir archivo
+	mov 	EBX, input_1  			;muevo al ebx el nombre de lo que quiero abrir
 
-	mov ecx, 2				;muevo permisos que tendra el archivo
+	mov 	ECX, 2				;muevo permisos que tendra el archivo
 	int 80h					;interrupcion del sistema
-	push eax				;salvo el FD
-	test eax, eax 			;primero nos aseguramos que abrio bien
+	push EAX				;salvo el FD
+	test EAX, EAX 				;primero nos aseguramos que abrio bien
 	js	error				;no es asi? imprime mensaje de error
-	mov	ebx, eax			;paso al ebx el FD
-	mov	ecx, archivo_1   	;paso el puntero del buffer con el archivo
+	mov	EBX, EAX			;paso al ebx el FD
+	mov	ECX, archivo_1   		;paso el puntero del buffer con el archivo y su len correspondiente
 
-	mov	edx, LenArchivo_1	;y su len correspondiente
-	mov	eax, sys_read		;y llamo a read de dicho archivo
+	mov	EDX, LenArchivo_1	
+	mov	EAX, sys_read			;y llamo a read de dicho archivo
 	int 80h					;interrupcion del sistema
-	pop ebx					;saco el FD de la pila
-	push eax				;salvo la cantidad de digitos
-	mov eax, sys_close 		;muevo la directiva de cerrar el archivo
-	int 80h	
+	pop EBX					;saco el FD de la pila
+	push EAX				
+	mov 	EAX, sys_close 			;muevo la directiva de cerrar el archivo
+	int 80h					;interrupcion del sistema
 
 
 
-;preguntar por el nombre del segundo archivo	
+;;;;preguntar por el nombre del segundo archivo;;;;	
 Preguntar_Nombre_2:
-	mov ecx, msg2			;muevo al ecx el mensaje al usuario
-	mov edx, lenmsg2	    ;muevo al edx el largo
-	call MostrarTexto  		;llamo al procedimiento de imprimir en pantalla
+	mov 	ECX, msg2		;muevo al ecx el mensaje al usuario
+	mov 	EDX, lenmsg2	    	;muevo al edx el largo
+	call MostrarTexto  		;llamo al procedimiento 
 
 	;leo de usuario un texto
-	mov ecx, input_2		;muevo al ecx el puntero del buffer de almacenamiento
+	mov 	ECX, input_2		;muevo al ecx el puntero del buffer de almacenamiento
 
-	mov edx, lenInput_2 	;muevo la catidad de caracteres maxima q puedo tener
+	mov 	EDX, lenInput_2 	;muevo la catidad de caracteres maxima q puedo tener
 
-	call LeerTexto 			;llamo al procedimiento de recibir de teclado
-	dec eax 				;en el eax tengo el largo del mensaje, lo decremento
-	mov byte[input_2+eax],0h	;cambio el ultimo caracter por un null
+	call LeerTexto 			;llamo al procedimiento 
+	dec EAX 			;en el eax tengo el largo del mensaje, lo decremento
+	mov byte[input_2+EAX],0h	;cambio el ultimo caracter por un null
 
 
 
-;abrir el archivo 
+;;;;abrir el archivo;;;; 
 .Abrir_archivo_2:			
-	mov eax, sys_open 		;muevo al eax la directiva de abrir archivo
-	mov ebx, input_2	 	;muevo al ebx el nombre de lo que quiero abrir
+	mov 	EAX, sys_open 		;muevo al eax la directiva de abrir archivo
+	mov 	EBX, input_2	 	;muevo al ebx el nombre de lo que quiero abrir
 
-	mov ecx, 2				;muevo permisos que tendra el archivo
+	mov 	ECX, 2			;muevo permisos que tendra el archivo
 	int 80h		      		;interrupcion del sistema
-	push eax				;salvo el FD
-	test eax, eax 			;primero nos aseguramos que abrio bien
-	js	error				;no es asi? imprime mensaje de error
-	mov	ebx, eax			;paso al ebx el FD
-	mov	ecx, archivo_2	 	;paso el puntero del buffer con el archivo
-	mov	edx, LenArchivo_2	;y su len correspondiente
-	mov	eax, sys_read		;y llamo a read de dicho archivo
-	int 80h					;interrupcion del sistema
-	pop ebx					;saco el FD de la pila
-	push eax				;salvo la cantidad de digitos
-	mov eax, sys_close 		;muevo la directiva de cerrar el archivo
-	int 80h		
+	push EAX			;salvo el FD
+	test EAX, EAX 			;primero nos aseguramos que abrio bien
+	js	error				
+	mov	EBX, EAX		;paso al ebx el FD
+	mov	ECX, archivo_2	 	;paso el puntero del buffer con el archivo y su len correspondiente
+	mov	EDX, LenArchivo_2	
+	mov	EAX, sys_read		;llamo a read de dicho archivo
+	int 80h				;interrupcion del sistema
+	pop EBX				;saco el FD de la pila
+	push EAX			;salvo la cantidad de digitos
+	mov 	EAX, sys_close 		;muevo la directiva de cerrar el archivo
+	int 80h				;interrupcion del sistema
 
 MostrarTexto:
-    mov     eax, sys_write
-    mov     ebx, stdout
+    mov     EAX, sys_write		;escribe 
+    mov     EBX, stdout
     int     80h 
     ret
 
 
 LeerTexto:
-    mov ebx, stdin
-    mov eax, sys_read
-    int 80H
+    mov 	EBX, stdin
+    mov 	EAX, sys_read		;read
+    int 80h				;interrupcion del sistema
     ret
 
 
 
 ;mostrar en pantalla si ocurrio un error al abrir el archivo
 error:
-	mov ecx, errorMsg
-	mov edx, lenErrorMsg
+	mov 	ECX, errorMsg
+	mov 	EDX, lenErrorMsg
 	call MostrarTexto
 	jmp Cerrar
 
@@ -159,53 +158,49 @@ error:
 
 ;llena la primera fila de ceros en la matriz
 fila_fill_zero:
-	pop eax			;saco el largo del 2do archivo
-	pop edx			;saco el largo del primer archivo
-	push edx		;los vuelvo a salvar
-	push eax
-	mov ecx, 0		;limpio el ecx para usarlo como contador
+	pop EAX			;saco el largo del 2do archivo
+	pop EDX			;saco el largo del primer archivo
+	push EDX		;los vuelvo a salvar
+	push EAX
+	mov 	ECX, 0		;limpio el ecx para usarlo como contador
 
 
 cycle:
-	mov byte[matriz+ecx], 0		;muevo a la matriz un cero
-	inc ecx						;incremento el contador
-	cmp ecx, edx				;pregunto si ya llene la primera fila
-	jne cycle					;si no lo he llenada, siga llenando
-	pop ebx		     			;saco el largo del 2do archivo
-	pop eax						;saco el largo del primer archivo
-	push eax					;los vuelvo a salvar
-	push ebx
-	xor edx, edx				
-	mul ebx           			;multiplico los largos para sacar el area de la matriz
+	mov byte[matriz+ECX], 0			;muevo a la matriz un cero
+	inc ECX					;incremento el contador
+	cmp ECX, edx				;pregunto si ya llene la primera fila
+	jne cycle				;por si no se ha llenado
+	pop EVX		     			;saco el largo del 2do archivo
+	pop EAX					;saco el largo del primer archivo
+	push EAX				;los vuelvo a salvar
+	push EBX
+	xor EDX, EDX				;limpio el edx				
+	mul EBX           			;multiplico para sacar el area de la matriz
 
-	mov edx, eax				;muevo al edx el area
+	mov 	EDX, EAX			;muevo al edx el area
 
 
 
 ;llena de ceros la primera columna de la matriz
 col_fill_zero:
-	mov byte[matriz+ecx], 0   	;muevo a la matriz un cero
-	add ecx, eax				;sumo al contador para que quede inicie en la siguiente columna
+	mov byte[matriz+ECX], 0   		;muevo a la matriz un cero
+	add ECX, EAX				;sumo al contador para que quede iniciado en la siguiente columna
 
-	cmp ecx, edx				;pregunto si ya termino
-	jne col_fill_zero			;si no lo es sigo llenando
+	cmp ECX, EDX				;pregunto si ya termino
+	jne col_fill_zero			;por si no se ha llenado
 	
-	pop eax						;saco el largo del 2do archivo
-	pop ecx						;saco el largo del primer archivo
-	push ecx					;los vuelvo a salvar
-	push eax
-	inc ecx						;incremento el tamaño de la fila para quedar en la pos [1,1] de la matriz
+	pop EAX					;saco el largo del 2do archivo
+	pop ECX					;saco el largo del primer archivo
+	push ECX				;los vuelvo a salvar
+	push EAX
+	inc ECX					;incremento el tamaño de la fila para quedar en la posicion [1,1] de la matriz
 
 
-
-;---------------------------------------------------------------------------------------------------
-
-
-
+;;;para cerrar;;;;
 Cerrar:
-	mov edx,1
-	mov ecx, enter
+	mov 	EDX,1
+	mov 	ECX, enter
 	call MostrarTexto
-	mov eax, sys_exit  			;muevo la variabla sys_close al eax
-	int 80h		  				;llamo a la interrupcion de kernel
+	mov 	EAX, sys_exit  			;muevo la variable sys_close al eax
+	int 80h		  			;llamo a la interrupcion de kernel
 
